82bd339fa12ccc125d119f95f010e774ca09606e
David <david.tondorf@tu-dortmund.de>
2018-03-29 11:55:46 +0200

    drivertest app: toggle gpio using vgpiofront

--- drivertest.c
+++ drivertest.c
@@ -25,44 +25,56 @@ unsigned cycles_low, cycles_high, cycles_low1, cycles_high1;
 unsigned long start_ts, end_ts;
 int running = 1;
 
-struct vgpiofront_dev *gpio;
-//~ struct vgpiofront_dev *gpio2;
+struct vgpiofront_dev *gpio_dev;
+unsigned int gpioLED = 403;       	// Linux 403 = Up 37
+unsigned int gpioFan = 464;       	// Linux 464 = Up 27
 
 void run_client(void *p)
 {
 	uint64_t t1 = 0, t2 = 0, us = 0, ns = 0;
+	int ret = 0;
+	unsigned int ledOn = 0;
 	
-	//~ struct netfront_dev* net;
     tprintk("Drivertest!\n");
     
  
-	gpio = init_vgpiofront("device/vgpio/0");
-	//~ gpio2 = init_vgpiofront("device/vgpio/1");
-	//~ net = init_netfront(NULL, NULL, NULL, NULL);
-    
-	tprintk("Drivertest running!\n");
+	gpio_dev = init_vgpiofront("device/vgpio/0");
 	
+	ret = gpio_request(gpio_dev, gpioLED, NULL);
+	tprintk("gpio_request: %d\n", ret);
+	ret = gpio_request(gpio_dev, gpioFan, NULL);
+	tprintk("gpio_request: %d\n", ret);
 	
+	ret = gpio_direction_output(gpio_dev, gpioLED, 0);
+	tprintk("gpio_direction_output: %d\n", ret);
+	
+	ret = gpio_direction_output(gpio_dev, gpioFan, 0);
+	tprintk("gpio_direction_output: %d\n", ret);
+	
+	gpio_set_value(gpio_dev, gpioFan, 1);
 	
     while (running == 1) {
 		t1 = NOW();
-        vgpiofront_send(gpio);
+		gpio_set_value(gpio_dev, gpioLED, ledOn);
         t2 = NOW();
         us = (t2-t1)/1000;
         ns = (t2-t1) - (us*1000);
         tprintk("Elapsed: %lu,%luus\n", us,ns);
+        ledOn = !ledOn;
         msleep(200);
         //~ msleep(1500);
         //~ vgpiofront_send(gpio2);
     }
-    shutdown_vgpiofront(gpio);
+    shutdown_vgpiofront(gpio_dev);
     do_exit();
 }
 
 #ifdef CONFIG_XENBUS
 void app_shutdown(unsigned reason)
 {
-	shutdown_vgpiofront(gpio);
+	gpio_free(gpio_dev, gpioLED);
+	gpio_free(gpio_dev, gpioFan);
+	shutdown_vgpiofront(gpio_dev);
 	//~ shutdown_vgpiofront(gpio2);
     struct sched_shutdown sched_shutdown = { .reason = reason };
     printk("Drivertest is shutting down: %d\n", reason);
